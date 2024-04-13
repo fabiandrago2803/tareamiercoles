9eef2847b42863fd81ea3151abbfb6855aeda3c8
fabiandrago2803 <fabiandragonetti@gmail.com>
2024-04-12 21:34:14 -0300

    otro

--- scripts.js
+++ scripts.js
@@ -2,19 +2,17 @@ const formulario = document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input');
 
 const expresiones = {
-	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
 	password: /^.{4,12}$/, // 4 a 12 digitos.
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+	
 }
 
 const campos = {
-	usuario: false,
 	nombre: false,
 	password: false,
 	correo: false,
-	telefono: false
+	
 }
 
 const validarFormulario = (e) => {
@@ -86,24 +84,17 @@ inputs.forEach((input) => {
 });
 
 
-
 formulario.addEventListener('submit', (e) => {
 	e.preventDefault();
 
 
-	const terminos = document.getElementById('terminos');
-	if(campos.usuario && campos.nombre && campos.password && campos.correo && campos.telefono && terminos.checked ){
-		formulario.reset();
 
 		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
 		setTimeout(() => {
 			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
 		}, 5000);
 
-		document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
-			icono.classList.remove('formulario__grupo-correcto');
-		});
+		
 	} else {
 		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
-	}
-});
+	})
