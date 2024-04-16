aeab76817fa88763c834af041385cec11ed6cbe5
fabiandrago2803 <fabiandragonetti@gmail.com>
2024-04-16 06:33:30 -0300

    formulario para validar

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+//Evita el envío del formulario hasta comprobar
+document.formtarea.addEventListener('submit', validarFormulario);
+function validarFormulario(evObject) {
+evObject.preventDefault();
+}
+
+
+    
+
+// evitar el submit al presionar enter
+document.addEventListener("keydown", function(e) {
+    if (e.key === "Enter") {
+      e.preventDefault();
+    }
+  });
+
 
 
 
