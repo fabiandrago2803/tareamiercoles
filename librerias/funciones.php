be15aebdf297eef3661928db1a4a921e7b413fdf
fabiandrago2803 <fabiandragonetti@gmail.com>
2024-04-30 20:39:27 -0300

    cambiossql

--- librerias/funciones.php
+++ librerias/funciones.php
@@ -7,10 +7,29 @@ function insertarRegistro()
 //echo 'Entro bien';
 global $con;
 $UserName= $_POST['UName'];
+$UserApellido= $_POST['UApellido'];
+$UserDni= $_POST['UDni'];
+$UserFecha= $_POST['UF_nacimiento'];
+$UserGenero= $_POST['UGenero'];
+$UserEmail= $_POST['UEmail'];
+$UserPassword= $_POST['UPassword'];
+$UserPassword2= $_POST['UPassword2'];
+
 
 $query= "Insert Into basedatostarea.sql (nombre,apellido,dni,f_nacimiento,genero,email,password,password2) values('$UserName'), " 
 
 
+
+$result= mysqli_query($con,$query);
+
+        if($result)
+        {
+            echo ' El registro se creo exitosamente';
+        }
+        else
+        {
+            echo 'Por favor chequear la Query';
+        }
 
 }
     
