dc2d47b2e09ad3f8a7ee4a199547e8b7542d918a
fabiandrago2803 <fabiandragonetti@gmail.com>
2024-05-08 19:43:55 -0300

    sumadas tablas sql

--- mangialavori ejemplo/fecha.sql
+++ mangialavori ejemplo/fecha.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 09-05-2024 a las 00:38:43
+-- Tiempo de generación: 09-05-2024 a las 00:43:33
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.1.2
 
@@ -20,33 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `testing`
 --
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `email`
---
-
-CREATE TABLE `email` (
-  `id` int(11) NOT NULL,
-  `valor` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `email`
---
-
-INSERT INTO `email` (`id`, `valor`) VALUES
-(1, 'fabiandasda@dsdsss'),
-(2, 'fabi11.25@123.com'),
-(3, ''),
-(4, 'hola****@gmail.gmail'),
-(5, 'fab@fabian@hotmail.com.ar'),
-(6, '.@yahoo.com.ar'),
-(7, 'fabo28/03@gmail.ar'),
-(8, '1111@222.com'),
-(9, 'faba!!@yahoo.com'),
-(10, 'fafafa@yo.123.com');
 
 -- --------------------------------------------------------
 
@@ -80,12 +53,6 @@ INSERT INTO `fecha` (`id`, `valor`) VALUES
 --
 
 --
--- Indices de la tabla `email`
---
-ALTER TABLE `email`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indices de la tabla `fecha`
 --
 ALTER TABLE `fecha`
@@ -94,12 +61,6 @@ ALTER TABLE `fecha`
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
-
---
--- AUTO_INCREMENT de la tabla `email`
---
-ALTER TABLE `email`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `fecha`
