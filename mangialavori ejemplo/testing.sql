1648de9329f02987c7c9be6c00643f5b810ff387
fabiandrago2803 <fabiandragonetti@gmail.com>
2024-05-08 19:39:37 -0300

    sumado testingsql

---
+++ mangialavori ejemplo/testing.sql
@@ -0,0 +1,113 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 09-05-2024 a las 00:38:43
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `testing`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `email`
+--
+
+CREATE TABLE `email` (
+  `id` int(11) NOT NULL,
+  `valor` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `email`
+--
+
+INSERT INTO `email` (`id`, `valor`) VALUES
+(1, 'fabiandasda@dsdsss'),
+(2, 'fabi11.25@123.com'),
+(3, ''),
+(4, 'hola****@gmail.gmail'),
+(5, 'fab@fabian@hotmail.com.ar'),
+(6, '.@yahoo.com.ar'),
+(7, 'fabo28/03@gmail.ar'),
+(8, '1111@222.com'),
+(9, 'faba!!@yahoo.com'),
+(10, 'fafafa@yo.123.com');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `fecha`
+--
+
+CREATE TABLE `fecha` (
+  `id` int(11) NOT NULL,
+  `valor` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `fecha`
+--
+
+INSERT INTO `fecha` (`id`, `valor`) VALUES
+(1, '1999-12-04'),
+(2, '0000-00-00'),
+(3, '0000-00-00'),
+(4, '0000-00-00'),
+(5, '0000-00-00'),
+(6, '0000-00-00'),
+(7, '0000-00-00'),
+(8, '0000-00-00'),
+(9, '0000-00-00'),
+(10, '0000-00-00');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `email`
+--
+ALTER TABLE `email`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `fecha`
+--
+ALTER TABLE `fecha`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `email`
+--
+ALTER TABLE `email`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de la tabla `fecha`
+--
+ALTER TABLE `fecha`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
